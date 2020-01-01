baf3f901626dab1514bcd8d111d6981da5004a5b
jonesbr <brandan.jones@uc.edu>
2020-01-01 17:46:28 -0500

    Initial commit

---
+++ app/src/main/java/app/plantdiary/myplantdiaryktprep/MainActivity.kt
@@ -0,0 +1,19 @@
+package app.plantdiary.myplantdiaryktprep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.plantdiary.myplantdiaryktprep.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
