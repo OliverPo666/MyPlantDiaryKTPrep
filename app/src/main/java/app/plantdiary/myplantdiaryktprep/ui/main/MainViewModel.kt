3c6df83f891674e69cd3d76c1452d50f10236060
jonesbr <brandan.jones@uc.edu>
2020-01-05 19:10:13 -0500

    More LiveData

--- app/src/main/java/app/plantdiary/myplantdiaryktprep/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/myplantdiaryktprep/ui/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package app.plantdiary.myplantdiaryktprep.ui.main
 
+import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +9,7 @@ class MainViewModel : ViewModel() {
     private val _names = MutableLiveData<String>()
 
     val names : LiveData<String>
+        @Bindable
         get()  {
             _names.setValue("Gloria");
             return _names
