f8e8ab69d3d39d17db43d0624c4569fa6b24781a
jonesbr <brandan.jones@uc.edu>
2020-01-06 22:11:31 -0500

    Final Autocomplete: Override toString.

--- app/src/main/java/app/plantdiary/myplantdiaryktprep/dto/Plant.kt
+++ app/src/main/java/app/plantdiary/myplantdiaryktprep/dto/Plant.kt
@@ -2,5 +2,8 @@ package app.plantdiary.myplantdiaryktprep.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Plant(@SerializedName("id") var plantId: Int = 0, var genus : String, var cultivar: String, var common:String) {
+data class Plant(@SerializedName("id") var plantId: Int = 0, var species: String, var genus : String, var cultivar: String, var common:String) {
+    override fun toString(): String {
+        return "$common $genus $species $cultivar $plantId"
+    }
 }
