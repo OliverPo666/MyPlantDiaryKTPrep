d026dc6dfcf9b3e5d06380a6296b6d2728b0c8b7
jonesbr <brandan.jones@uc.edu>
2020-01-15 18:53:49 -0500

    GPS Test paritally works... commit and then back out differences.

--- app/src/androidTest/java/app/plantdiary/myplantdiaryktprep/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/plantdiary/myplantdiaryktprep/ExampleInstrumentedTest.kt
@@ -1,5 +1,7 @@
 package app.plantdiary.myplantdiaryktprep
 
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +9,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +19,26 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("app.plantdiary.myplantdiaryktprep", appContext.packageName)
+    }
+
+    @Test
+    fun testLocationUpdates() {
+        // TODO mock application here... or get it from context.
+        var locationVm = LocationViewModel(InstrumentationRegistry.getInstrumentation().targetContext)
+        locationVm.getLocationData().observeForever {
+            assertTrue(it.latitude.toDouble() > -90)
+            assertTrue(it.longitude.toDouble() > -180)
+
+        }
+
     }
 }
--- app/src/main/java/app/plantdiary/myplantdiaryktprep/LocationViewModel.kt
+++ app/src/main/java/app/plantdiary/myplantdiaryktprep/LocationViewModel.kt
@@ -1,10 +1,12 @@
 package app.plantdiary.myplantdiaryktprep
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 
-class LocationViewModel(application: Application) : AndroidViewModel(application) {
+class LocationViewModel(context: Context) : ViewModel() {
 
-    private val locationData = LocationLiveData(application)
+    private val locationData = LocationLiveData(context)
     fun getLocationData() = locationData;
 }
