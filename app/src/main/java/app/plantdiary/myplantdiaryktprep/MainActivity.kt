d63bb0434cbbd2f756e66382e76eb41f0b8fabba
jonesbr <brandan.jones@uc.edu>
2020-03-20 22:27:23 -0400

    Implement gestures.

--- app/src/main/java/app/plantdiary/myplantdiaryktprep/MainActivity.kt
+++ app/src/main/java/app/plantdiary/myplantdiaryktprep/MainActivity.kt
@@ -2,11 +2,16 @@ package app.plantdiary.myplantdiaryktprep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.MotionEvent
+import android.widget.Toast
+import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.MotionEventCompat
 import app.plantdiary.myplantdiaryktprep.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var detector : GestureDetectorCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,34 +21,72 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        detector = GestureDetectorCompat(this, DiaryGestureListener())
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         val action = event?.actionMasked
 
-        return when (action) {
-            MotionEvent.ACTION_DOWN -> {
-                true
-            }
-            MotionEvent.ACTION_MOVE -> {
-                true
-            }
-            MotionEvent.ACTION_UP -> {
-                true
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                true
-            }
-            MotionEvent.ACTION_OUTSIDE -> {
-                true
-            }
-
-            else -> super.onTouchEvent(event)
+        return if (detector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
         }
-
-
 
     }
 
+    inner class DiaryGestureListener : GestureDetector.SimpleOnGestureListener() {
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+
+        override fun onFling(
+            downEvent: MotionEvent?,
+            moveEvent: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+            // is it more up/down, or more left/right?
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        this@MainActivity.onSwipeRight()
+                    } else {
+                        this@MainActivity.onSwipeLeft()
+                    }
+                }
+            } else {
+                // up or down
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        this@MainActivity.onSwipeBottom()
+                    } else {
+                        this@MainActivity.onSwipeTop()
+                    }
+                }
+            }
+
+            return super.onFling(downEvent, moveEvent, velocityX, velocityY)
+        }
+
+    }
+
+    private fun onSwipeTop() {
+        Toast.makeText(this, "Swipe Top", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeBottom() {
+        Toast.makeText(this, "Swipe Bottom", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeLeft() {
+        Toast.makeText(this, "Swipe Left", Toast.LENGTH_LONG).show()
+    }
+
+    internal fun onSwipeRight() {
+        Toast.makeText(this, "Swipe Right", Toast.LENGTH_LONG).show()
+    }
 
 }
